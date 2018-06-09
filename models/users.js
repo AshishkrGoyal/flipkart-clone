6cd4c51b73206e621e87620faa3aac6ae0314e5d
AshishkrGoyal <15103140ashish@gmail.com>
2018-06-09 16:26:26 +0530

    database-connected

--- models/users.js
+++ models/users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
 
 const Schema = mongoose.Schema;
 
@@ -28,3 +30,16 @@ const UserSchema = new Schema(
     }
   }
 )
+
+//hashing password for security purposre
+
+UserSchema.methods.encryptPassword = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10),null)
+}
+
+
+UserSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+module.exports = mongoose.model('User',UserSchema)
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
+const User = require('./models/users');
+
+mongoose.connect('mongodb://localhost:27017/flipkart', function (err) {
+  if(err)
+  {
+    console.log(err);
+  }
+  else
+  {
+    console.log("database has been connected!!");
+  }
+});
 
 const app = express();
 
